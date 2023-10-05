ef3850bd16caec9e1a4fcea959f725cce293f9ab
meshackeliyas <meshackeliyas@elgi.com>
2023-10-05 17:39:38 +0530

    Test IOS

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
-import device_info_plus
+import device_info_plus_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
 import pdfx
